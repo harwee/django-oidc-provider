51ff1aad313fdde2dfc0af277affe882d652f2b7
SriHarsha Gangisetty <sriharshagangisetty@gmail.com>
2019-07-26 18:07:01 +0530

    allow skipping consent for public clients

--- oidc_provider/views.py
+++ oidc_provider/views.py
@@ -103,21 +103,12 @@ class AuthorizeView(View):
                     raise AuthorizeError(
                         authorize.params['redirect_uri'], 'consent_required', authorize.grant_type)
 
-                implicit_flow_resp_types = {'id_token', 'id_token token'}
-                allow_skipping_consent = (
-                    authorize.client.client_type != 'public' or
-                    authorize.params['response_type'] in implicit_flow_resp_types)
-
-                if not authorize.client.require_consent and (
-                        allow_skipping_consent and
-                        'consent' not in authorize.params['prompt']):
+                if not authorize.client.require_consent and ('consent' not in authorize.params['prompt']):
                     return redirect(authorize.create_response_uri())
 
                 if authorize.client.reuse_consent:
                     # Check if user previously give consent.
-                    if authorize.client_has_user_consent() and (
-                            allow_skipping_consent and
-                            'consent' not in authorize.params['prompt']):
+                    if authorize.client_has_user_consent() and ('consent' not in authorize.params['prompt']):
                         return redirect(authorize.create_response_uri())
 
                 if 'none' in authorize.params['prompt']:
